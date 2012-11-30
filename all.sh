a6ee009bfea51c78a90f55781ef7678398ea37ef
Peter Bailey <peter.eldridge.bailey@gmail.com>
2012-11-29 20:15:13 -0700

    update header

--- all.sh
+++ all.sh
@@ -2,5 +2,5 @@
 
 ls *.dat | sort | parallel 'echo;echo -n {}" "; ./stats.R {}' | tee periodTable
 
-(echo file delmean meanPeriod delsd sd;sed -e '/^$/d' periodTable) | grep mean > periodTable.dat
+(echo file delmean meanPeriod delsd sd del median del min del max del offsetTime;sed -e '/^$/d' periodTable) | grep mean > periodTable.dat
 
