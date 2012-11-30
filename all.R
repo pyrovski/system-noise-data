6e2e75a1ca20db0b2c5212f762c5c2cafb035623
Peter Bailey <peter.eldridge.bailey@gmail.com>
2012-11-29 20:14:58 -0700

    split names into node + rank, pdf plots

--- all.R
+++ all.R
@@ -1,15 +1,27 @@
 #!/usr/bin/env Rscript
 
+pdfs=T
+
+source('statsFuncs.R')
+
 periodTable = read.table('periodTable.dat', header=T )
 periodTable = periodTable[,-grep('^del', names(periodTable))]
+
+splitNames = sapply(as.character(periodTable$file), strsplit, '[.]')
+periodTable$node = as.numeric(sub('rzmerl', '', sapply(splitNames, '[[', 1)))
+periodTable$rank = as.numeric(sub('rzmerl', '', sapply(splitNames, '[[', 2)))
+
+periodTable = periodTable[order(periodTable$node, periodTable$rank),]
 
 periodSdCdf = ecdf(periodTable$sd)
 sdThreshold = 10*median(periodTable$sd, na.rm=T)
 
 sel = periodTable$sd <= sdThreshold
 
-dev.new()
-hist(periodTable$mean[sel])
+newDev('mean_period_histogram.pdf')
+hist(periodTable$meanPeriod[sel])
+if(pdfs) dev.off()
 
-dev.new();
-hist(periodTable$sd[sel])
+#newDev()
+#hist(periodTable$sd[sel])
+
